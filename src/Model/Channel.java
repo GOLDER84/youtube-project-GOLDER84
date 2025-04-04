66ee08e87fc83e21d21eaddd3bbb889386a32dfe
Aref Zargar <aref.unlimited.84@gmail.com>
2025-04-04 23:57:44 +0330

    Channel class created.

---
+++ src/Model/Channel.java
@@ -0,0 +1,70 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Channel {
+    private static int idCounter = 0;
+    private int channelId;
+    private String channelName;
+    private String channelDescription;
+    private String channelCover;
+    private int channelOwnerId;
+    private ArrayList<Playlist> playlists;
+    private ArrayList<User> subscribers;
+    private ArrayList<Integer> subscribersList;
+    private ArrayList<Integer> contentId;
+
+    public Channel(String channelName, String channelDescription, String channelCover, int channelOwnerId) {
+        this.channelId= idCounter++;
+        this.channelName = channelName;
+        this.channelDescription = channelDescription;
+        this.channelCover = channelCover;
+        this.channelOwnerId = channelOwnerId;
+        this.playlists = new ArrayList<>();
+        this.subscribers = new ArrayList<>();
+        this.subscribersList = new ArrayList<>();
+        this.contentId = new ArrayList<>();
+    }
+    public int getChannelId() {
+        return channelId;
+    }
+    public void setChannelId(int channelId) {
+        this.channelId = channelId;
+    }
+    public String getChannelName() {
+        return channelName;
+    }
+    public void setChannelName(String channelName) {
+        this.channelName = channelName;
+    }
+    public String getChannelDescription() {
+        return channelDescription;
+    }
+    public void setChannelDescription(String channelDescription) {
+        this.channelDescription = channelDescription;
+    }
+    public String getChannelCover() {
+        return channelCover;
+    }
+    public void setChannelCover(String channelCover) {
+        this.channelCover = channelCover;
+    }
+    public int getChannelOwnerId() {
+        return channelOwnerId;
+    }
+    public void setChannelOwnerId(int channelOwnerId) {
+        this.channelOwnerId = channelOwnerId;
+    }
+    public ArrayList<Playlist> getPlaylists() {
+        return playlists;
+    }
+    public ArrayList<User> getSubscribers() {
+        return subscribers;
+    }
+    public ArrayList<Integer> getSubscribersList() {
+        return subscribersList;
+    }
+    public ArrayList<Integer> getContentId() {
+        return contentId;
+    }
+}
