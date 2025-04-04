c1ca79bdfeb670825c159a9961f3424de84dbddb
Aref Zargar <aref.unlimited.84@gmail.com>
2025-04-05 00:15:04 +0330

    ShortVideo class created.

---
+++ src/Model/ShortVideo.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class ShortVideo extends Video {
+    private String referenceMusic;
+
+    public ShortVideo(int ownerId, int channelId, String name, String description, int duration, Category category, String fileLink, String cover, String subtitles , String referenceMusic) {
+        super(ownerId, channelId, name, description, duration, category, fileLink, cover, subtitles);
+        this.referenceMusic = referenceMusic;
+    }
+    public String getReferenceMusic() {
+        return referenceMusic;
+    }
+    public void setReferenceMusic(String referenceMusic) {
+        this.referenceMusic = referenceMusic;
+    }
+}
