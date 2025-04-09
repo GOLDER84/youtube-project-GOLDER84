bc4e07c1e51ffafe1fbbf8d548471ac8d9e24ed2
Aref Zargar <aref.unlimited.84@gmail.com>
2025-04-10 00:18:56 +0330

    VideoController class completed.

--- src/Model/Admin.java
+++ src/Model/Admin.java
@@ -4,7 +4,7 @@ public class Admin extends Account{
     private static Admin instance;
 
     public Admin() {
-        super("admin", "admin123", "System Admin",
+        super("Admin", "123", "System Admin",
                 "admin123@gmail.com", "1234567890", "default_admin_cover.jpg");
     }
 
