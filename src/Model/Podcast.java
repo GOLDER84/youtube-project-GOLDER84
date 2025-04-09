fa59c221b8b3438297b207718277cd57c2cb71ea
Aref Zargar <aref.unlimited.84@gmail.com>
2025-04-10 00:24:17 +0330

    Podcast class completed.

--- src/Model/Podcast.java
+++ src/Model/Podcast.java
@@ -1,19 +1,11 @@
 package Model;
 
 public class Podcast extends Content{
-    private User ownerOfPodcast;
     private boolean isExclusive;
 
-    public Podcast(int ownerId, int channelId, String name, String description, int duration, Category category, String fileLink, String cover ,User ownerOfPodcast , boolean isExclusive) {
-        super(ownerId, channelId, name, description, duration, category, fileLink, cover);
-        this.ownerOfPodcast = ownerOfPodcast;
+    public Podcast(int ownerId, int channelId, String name, String description, String duration, Category category, String fileLink, String cover , boolean isExclusive) {
+        super(ownerId, channelId, name, isExclusive,description, duration, category, fileLink, cover);
         this.isExclusive = isExclusive;
-    }
-    public User getOwnerOfPodcast() {
-        return ownerOfPodcast;
-    }
-    public void setOwnerOfPodcast(User ownerOfPodcast) {
-        this.ownerOfPodcast = ownerOfPodcast;
     }
     public boolean isExclusive() {
         return isExclusive;
