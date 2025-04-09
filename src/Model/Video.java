50fbc6dd2d96d6c47289f18ee11e3dce1ee8f129
Aref Zargar <aref.unlimited.84@gmail.com>
2025-04-10 00:29:35 +0330

    Video class completed.

--- src/Model/Video.java
+++ src/Model/Video.java
@@ -3,8 +3,8 @@ package Model;
 public abstract class Video extends Content{
     protected String subtitles;
 
-    public Video(int ownerId, int channelId, String name, String description, int duration, Category category, String fileLink, String cover , String subtitles) {
-        super(ownerId, channelId, name, description, duration, category, fileLink, cover);
+    public Video(int ownerId, int channelId, String name , boolean isExclusive, String description, String duration, Category category, String fileLink, String cover , String subtitles) {
+        super(ownerId, channelId, name, isExclusive ,description, duration, category, fileLink, cover);
         this.subtitles = subtitles;
     }
     public String getSubtitles() {
